d1426172ef3434e2bf5a7a7697da65f896161728
xiangbinp <xiangbin.poh@gmail.com>
2020-11-28 00:12:28 +0800

    added db codes for testing

--- LecturerController.py
+++ LecturerController.py
@@ -1,3 +1,21 @@
+#from flask_mysqldb import MySQL
+import mysql.connector
+from mysql.connector import errorcode
+# Test DB connector
+cnx = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='2x01')
+cursor = cnx.cursor()
+
+query =("SELECT * FROM user")
+
+cursor.execute(query)
+
+for user in cursor:
+    print(user)
+
+cursor.close()
+cnx.close()
+
+
 class LecturerController:
     def viewClass(self):
         pass

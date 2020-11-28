2ce969ca103e56622f51f4b87722a61c0e554c6c
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2020-11-28 23:39:51 +0800

    Revert "Updated Authenticate to use AuthenticateFactory Class"
    This reverts commit 1e48885baef906ae8b6efde7acbdc60afbd5ca06.

--- Account.py
+++ Account.py
@@ -1,47 +1,22 @@
-import mysql, MySQLdb
-from flask import Flask
-from flask_mysqldb import MySQL, MySQLdb
-
-app = Flask(__name__)
-
-# Database Config
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_DB'] = '2101project'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['SECRET_KEY'] = b'6hc/_psp,./;2ZZx3c6_s,1//'
-
-mysql = MySQL(app)
-
-
 # account factory need to verify user first? Actually do we really need an account factory, we not creating new classes
 class AccountFactory:
-    def __init__(self, uid):
-        self.uid = uid
-        self.valid = None
+    def __init__(self, id, name, rights=False):
+        self._id = id
+        self._name = name
+        self._rights = rights
+        self._idlen = len(str(self._id))
 
-        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute("SELECT * FROM user WHERE UID=" + self.uid)
-        user = cur.fetchone()
-        cur.close()
+        if self._idlen != 6 and self._idlen != 7:
+            raise ValueError("Invalid User ID")
 
-        if user:
-            self.valid = 1
-            self.name = str(user['name'])
-            if user['isStudent'] == 0:
-                self.role = 1  # Lecturer
-            else:
-                self.role = 0  # Student
-
-    def getuid(self):
-        return self.uid
+    def getid(self):
+        return self._id
 
     def getName(self):
-        return self.name
+        return self._name
 
-    def getRole(self):
-        return self.role
+    def execRights(self):
+        if self._idlen == 6:
+            self._rights = True
 
-    def getValid(self):
-        return self.valid
+

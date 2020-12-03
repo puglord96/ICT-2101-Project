a5f6b0e1b32d5bbfd08cfe4f8cf49ac97093af47
puglord96 <55310319+puglord96@users.noreply.github.com>
2020-12-03 15:39:52 +0800

    edited database.py class

--- Database.py
+++ Database.py
@@ -2,12 +2,12 @@ from flask_mysqldb import MySQL, MySQLdb
 
 class SQL():
 
-    def __init__(self,user, host ,dbname ,cursor ,secretkey,pw=""):
+    def __init__(self,user, host ,dbname ,cursor ,pw=""):
         app.config['MYSQL_USER'] = user
         app.config['MYSQL_PASSWORD'] = pw
         app.config['MYSQL_HOST'] = host
         app.config['MYSQL_DB'] = dbname
         app.config['MYSQL_CURSORCLASS'] = cursor
-        app.config['SECRET_KEY'] = secretkey
+        app.config['SECRET_KEY'] = b'6hc/_psp,./;2ZZx3c6_s,1//'
         return mySQL(app)
 
